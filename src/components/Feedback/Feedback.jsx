eade0326449b13b53f680177924c5ee8ae647239
Andrii D. <and05gt@gmail.com>
2024-09-06 21:35:33 +0200

    Add components and styles

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,15 @@
+import "./Feedback.css";
+
+const Feedback = () => {
+  return (
+    <div className="feedback-wrapper">
+      <span>Good: 0</span>
+      <span>Neutral: 0</span>
+      <span>Bad: 0</span>
+      <span>Total: 0</span>
+      <span>Positive: 0%</span>
+    </div>
+  );
+};
+
+export default Feedback;
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,14 @@
+import "./Options.css";
+
+const Options = () => {
+  return (
+    <div className="btn-wrapper">
+      <button type="button">Good</button>
+      <button type="button">Neutral</button>
+      <button type="button">Bad</button>
+      <button type="button">Reset</button>
+    </div>
+  );
+};
+
+export default Options;
